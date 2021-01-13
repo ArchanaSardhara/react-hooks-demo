604f467888ffa9dcec6a22c19606753d0820a677
Archana Sardhara <archnasardhara@gmail.com>
2021-01-13 11:35:47 +0530

    use state hook

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Card } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 import { HOOKLIST } from "./const";
 
-const Hooks = () => {
-    const { hook } = useParams();
-    const ActiveHook = HOOKLIST.find(h => `:${h.name}` === hook)
-    return (
-        <ActiveHook.Component />
-    )
+const Hooks = (props) => {
+  const { hook } = useParams();
+  const [ActiveHook, setActiveHook] = useState(null);
+
+  useEffect(() => {
+    const _hook = HOOKLIST.find(h => `:${h.name}` === hook);
+    setActiveHook(_hook);
+  }, [ActiveHook])
+
+  const redirectToHook = (data) => {
+    if (data) {
+      setActiveHook(null);
+      props.history.push(`/:${data.name}`);
+    } else {
+      props.history.push('/')
+    }
+  }
+  
+  if (!ActiveHook) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <Card>
+        <Card.Header>
+          <div className="d-flex justify-content-between">
+            <div className="d-flex align-items-center">
+              <h1 className="m-0 text-info">{ActiveHook.title}</h1>
+            </div>
+            <div className="d-flex align-items-center">
+              <Button variant="info" className="mx-2 my-1 d-flex align-items-center" onClick={() => redirectToHook(null)}>
+                <span className="back-icon">&#10158;</span> Back
+              </Button>
+              <Button variant="info" className="mx-2 my-1 d-flex align-items-center" onClick={() => redirectToHook(ActiveHook)}>
+                <span className="back-icon">&#8635;</span>  Refresh
+          </Button>
+            </div>
+          </div>
+        </Card.Header>
+        <Card.Body className="p-3">
+          <ActiveHook.Component />
+        </Card.Body>
+      </Card>
+    </div>
+  )
 }
 
 export default Hooks;
--- src/components/use-state.js
+++ src/components/use-state.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 
-const UseStateComponent = () => (
-    <div>UseStateComponent</div>
-)
+const UseStateComponent = () => {
+  const [count, setCount] = useState(0);
+
+  const updeteCount = (offSet) => {
+    if (offSet) {
+      setCount(count + offSet);
+    } else {
+      setCount(0);
+    }
+  }
+
+  return (
+    <div className="m-5 p-5 border border-info bg-light">
+      <h3 className="text-center text-info">Count: {count}</h3>
+      <div></div>
+      <div className="mt-5 d-flex justify-content-center">
+        <Button className="mx-2" variant="outline-success" onClick={() => updeteCount(1)}>Increase</Button>
+        <Button className="mx-2" variant="outline-secondary" onClick={() => updeteCount(0)}>Reset</Button>
+        <Button className="mx-2" variant="outline-danger" onClick={() => updeteCount(-1)}>Decrease</Button>
+      </div>
+    </div>
+  )
+}
 
 export default UseStateComponent;
