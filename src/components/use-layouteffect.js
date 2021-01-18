555ce3dc1bf0093c5317895c4d3dd2e6e243bd2b
Archana Sardhara <archnasardhara@gmail.com>
2021-01-18 15:27:08 +0530

    use layout effect hook

--- src/components/use-layouteffect.js
+++ src/components/use-layouteffect.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { Button } from "react-bootstrap";
 
 const UseLayoutEffectComponent = () => {
+  const [count, setCount] = useState(10);
+  const boxRef = useRef()
 
-    return (
-        <div>
-            UseLayoutEffectComponent
-        </div>
-    )
+  useLayoutEffect(() => {
+    boxRef.current.style.width = `${count}px`;
+    boxRef.current.style.height = `${count}px`;
+  })
+
+  const updeteCount = (offSet) => {
+    if (offSet) {
+      setCount(count + offSet);
+    } else {
+      setCount(10);
+    }
+  }
+
+  return (
+    <div className="m-5 p-5 border border-info bg-light d-flex flex-column justify-content-center align-items-center">
+      <div ref={boxRef} className="box bg-info mb-3"></div>
+      <h3 className="text-center text-info">Count: {count}</h3>
+      {count < 0 && <span className="text-danger">Reset the count to see visital effect on box.</span>}
+      <div className="mt-2 d-flex justify-content-center">
+        <Button className="mx-2" variant="outline-success" onClick={() => updeteCount(1)}>Increase</Button>
+        <Button className="mx-2" variant="outline-secondary" onClick={() => updeteCount(0)}>Reset</Button>
+        <Button className="mx-2" variant="outline-danger" onClick={() => updeteCount(-1)}>Decrease</Button>
+      </div>
+    </div>
+  )
 }
 
 export default UseLayoutEffectComponent;
