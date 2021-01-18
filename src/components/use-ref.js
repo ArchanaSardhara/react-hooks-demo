2d9ad2431c7aa3fbaa71f8ceb0b527be79a8026e
Archana Sardhara <archnasardhara@gmail.com>
2021-01-18 13:37:17 +0530

    use ref hook

--- src/components/use-ref.js
+++ src/components/use-ref.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
 
 const UseRefComponent = () => {
+  const inputEl = useRef(null);
+
+  const onButtonClick = () => {
+    inputEl.current.focus();
+  };
 
   return (
-    <div>
-      UseRefComponent
+    <div className="m-3 p-3 bg-light border border-info">
+      <div className="w-100 text-center">
+        <p>Used to give reference to user controled element.</p>
+        <p>Here on button click we use input reference to focus.</p>
+      </div>
+      <div className="m-3 p-3 d-flex justify-content-center">
+        <Form.Control className="w-25 mx-2" ref={inputEl} type="text" />
+        <Button variant="outline-info" onClick={onButtonClick}>Focus the input</Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default UseRefComponent;
